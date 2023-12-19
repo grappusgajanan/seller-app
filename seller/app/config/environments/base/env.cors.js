0cf6f36a792e4fc150f1957ea9a9309e3a24614b
Gajanan Patil <gajanan@grappus.com>
2023-12-19 16:08:57 +0530

    local-ref-setup

--- seller/app/config/environments/base/env.cors.js
+++ seller/app/config/environments/base/env.cors.js
@@ -1,9 +1,13 @@
 module.exports = {
-    'cors': {
-        'whitelistUrls': [
-            '*',
-            'http://localhost:3000',
-            'https://localhost:3000'
-        ]
-    }
+  cors: {
+    whitelistUrls: [
+      "*",
+      "http://localhost:3000",
+      "https://localhost:3000",
+      "http://localhost",
+      "https://localhost",
+      "http://localhost:80",
+      "https://localhost:80",
+    ],
+  },
 };
--- seller/app/modules/product/v1/middleware/api.params.validator.js
+++ seller/app/modules/product/v1/middleware/api.params.validator.js
@@ -1,27 +1,29 @@
-import { BadRequestParameterError } from '../../../../lib/errors';
+import { BadRequestParameterError } from "../../../../lib/errors";
 
 exports.middleware = (options) => async (req, res, next) => {
-    try {
-        let data;
+  try {
+    let data;
 
-        let { schema } = options;
+    let { schema } = options;
 
-        // Read data from request body
-        if (['POST', 'PUT', 'PATCH'].includes(req.method.toUpperCase())) {
-            data = req.body;
-        } else {
-            // Read data from request query params
-            data = req.params;
-        }
-
-        const { error } = schema.validate(data);
-        if (error) {
-            next(new BadRequestParameterError(error));
-        } else {
-            next();
-        }
-    } catch (err) {
-        console.log(err);
-        next(err);
+    // Read data from request body
+    if (["POST", "PUT", "PATCH"].includes(req.method.toUpperCase())) {
+      data = req.body;
+    } else {
+      // Read data from request query params
+      data = req.params;
     }
+    console.log("gajanan schema", schema);
+    console.log("gajanan data", data);
+    const { error } = schema.validate(data);
+    console.log("gajanan error", error);
+    if (error) {
+      next(new BadRequestParameterError(error));
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 };
